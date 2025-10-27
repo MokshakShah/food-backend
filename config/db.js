9611e8709fa443af111c2b73dc869b8b87637f2c
Mokshak Ketan Shah <mokshak18@gmail.com>
2025-10-27 17:18:59 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ dotenv.config(); // Load variables from .env
 
 export const connectDB = async () => {
   try {
+    console.log("MONGO_URI from Render ->", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("DB Connected");
   } catch (error) {
